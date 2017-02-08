2a07ccf6db94f57739220f3745430da44b1412d8
Joaquin A. Rodriguez Huerta <jrhuerta@alayacare.com>
2017-02-08 13:48:29 -0500

    Pivoting to S3 migration.

--- ceph2swift.py
+++ ceph2swift.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import argparse
-import functools
 import boto
 import boto.s3.connection
-import logging
 import os
-import swiftclient
+import signal
+import time
 
 
 class EnvDefault(argparse.Action):
@@ -22,30 +21,16 @@ class EnvDefault(argparse.Action):
         setattr(namespace, self.dest, values)
 
 
-class Pipeline(object):
-    def __init__(self, items=None, *stages):
-        self._items = items
-        for stage in stages:
-            self.add(stage)
-
-    def add(self, stage):
-        self._items = stage(self._items)
-        return self
-
-    def before_run(self):
-        pass
-
-    def after_run(self):
-        pass
-
-    def run(self):
-        self.before_run()
-        for _ in self._items:
-            pass
-        self.after_run()
-
-
 class Stage(object):
+    items = None
+
+    def __init__(self, items=None, **kwargs):
+        self.items = items
+        self.config = kwargs
+
+    def configure(self, **kwargs):
+        self.config.update(kwargs)
+
     def process(self, item):
         return item
 
@@ -55,9 +40,9 @@ class Stage(object):
     def after_process(self):
         pass
 
-    def __call__(self, items):
+    def __iter__(self):
         self.before_process()
-        for item in items:
+        for item in self.items:
             try:
                 yield self.process(item)
             except Exception as ex:
@@ -65,93 +50,130 @@ class Stage(object):
         self.after_process()
 
 
-class S3KeyInfo(Stage):
+class Pipeline(object):
+    items = None
+    start_time = 0
+    exit = False
+
+    def __init__(self, items, *stages, **kwargs):
+        self.items = items
+        self.config = kwargs
+        for stage in stages:
+            stage.configure(**kwargs)
+            self.add(stage)
+
+    def add(self, stage):
+        stage.items = self.items
+        self.items = stage
+
+    def before_process(self):
+        self.start_time = time.time()
+
+    def after_process(self):
+        print("*"*80)
+        print("Time elapsed: {}".format(time.time() - self.start_time))
+        pass
+
+    def __call__(self):
+        self.before_process()
+        for _ in self.items:
+            pass
+        self.after_process()
+
+
+class PrintFileInfo(Stage):
 
     def process(self, item):
         print('-'*80)
         print(item.name)
-        print("MD5:{}".format(item.etag[1:-1]))
+        print("SRC MD5: {}".format(item.etag[1:-1]))
         return item
+
+    def after_process(self):
+        print("*"*80)
 
 
 class Filter(Stage):
-    def __init__(self, name, filter):
+
+    def __init__(self, name, filter, **kwargs):
+        super(Filter, self).__init__(**kwargs)
         self.name = name
         self.filter = filter
 
-    def __call__(self, items):
-        for item in items:
-            if self.filter(item):
-                print("SKIPPED: by {} filter.".format(self.name))
-                continue
-            yield item
-
-
-class SwiftStage(Stage):
-
-    def __init__(self, connection, tenant):
-        self.connection = connection
-        self.tenant = tenant
-
-    @property
-    def container_name(self):
-        return 'alaya-{}'.format(self.tenant)
-
-    def __getattr__(self, item):
-        method = getattr(self.connection, item)
-        return functools.partial(method, self.container_name)
-
-
-class SwiftCreateTenantContainer(SwiftStage):
-
-    def before_process(self):
-        try:
-            self.put_container()
-            print("{}: Created container".format(self.container_name))
-        except Exception as ex:
-            logging.error("{}: Unable to create bucket.")
-            logging.exception(ex)
-            raise
-
-
-class SwiftCheckIfFileExists(SwiftStage):
-    def before_process(self):
-        #load all keys from swift to speed up checking if the file exists.
-        pass
-
     def process(self, item):
-        try:
-            headers = self.head_object(item.name)
-            assert item.etag[1:-1] != headers.get('etag'), "checksum verified."
-        except swiftclient.ClientException as ex:
-            if getattr(ex, 'http_status', None) != 404:
-                raise
+        if self.filter(item):
+            raise RuntimeError("by {} filter.".format(self.name))
         return item
 
 
-class SwiftCreateFolderStructure(SwiftStage):
-    def __init__(self, *args, **kwargs):
-        super(SwiftCreateFolderStructure, self).__init__(*args, **kwargs)
-        self.existing_folders = set()
+class S3Stage(Stage):
+
+    @property
+    def connection(self):
+        try:
+            return self.config['connection']
+        except KeyError:
+            raise RuntimeError('S3Stage: connection not configured.')
+
+    @property
+    def bucket_name(self):
+        try:
+            return 'alaya-{}'.format(self.config['tenant'])
+        except ValueError:
+            raise RuntimeError('S3Stage: tenant name not configured.')
+
+    @property
+    def bucket(self):
+        try:
+            return self.connection.get_bucket(self.bucket_name)
+        except Exception as ex:
+            raise ex
+            #raise RuntimeError('S3Stage: error getting bucket.')
+
+
+class S3CreateFolderStructure(S3Stage):
+
+    content_type = 'application/directory'
+    existing_folders = None
+    start_folder_count = 0
+
+    def load_existing_folders(self):
+        existing_folders = set()
+        print('Preloading existing folders ')
+        for key in self.bucket.list():
+            key = self.bucket.get_key(key.name)
+            if key.content_type == self.content_type:
+                existing_folders.add(key.name)
+        print('{} folders loaded.'.format(len(existing_folders)))
+        return existing_folders
 
     def sub_folders(self, filename):
         folders = filename.split('/')[:-1]
         current_path = ''
         for folder in folders:
-            current_path = current_path + ('/' if current_path else '') + folder
+            current_path = current_path + folder + '/'
             yield current_path
 
     def create_folder(self, path):
         try:
-            self.put_object(obj=path, contents=None,
-                            content_type='application/directory')
+            key = self.bucket.new_key(path)
+            key.content_type = self.content_type
+            key.set_contents_from_string('')
             self.existing_folders.add(path)
-            print('{}: folder created.'.format(path))
+            print(path)
         except Exception as e:
-            print(e.message)
-            raise
+            print("{}: {}".format(path, e.message))
+
+    def before_process(self):
+        self.existing_folders = self.load_existing_folders()
+        self.start_folder_count = len(self.existing_folders)
+
+    def after_process(self):
+        print("{} folder(s) created.".format(
+            len(self.existing_folders) - self.start_folder_count))
 
     def process(self, item):
+        print('FOLDERS:')
         for folder in self.sub_folders(item.name):
             if folder in self.existing_folders:
                 continue
@@ -159,95 +181,107 @@ class SwiftCreateFolderStructure(SwiftStage):
         return item
 
 
-class SwiftUploadFile(SwiftStage):
+class S3UploadFile(S3Stage):
+
+    key_count = 0
 
     def process(self, item):
-        self.put_object(
-            item.name,
-            contents=item.get_contents_as_string(),
-            content_type=item.content_type
-        )
-        headers = self.head_object(item.name)
-        assert headers.get('etag') == item.etag[1:-1], \
-            'Checksum check failed.'
-        print('Checksum check passed.')
+        key = self.bucket.get_key(item.name)
+        if key:
+            assert item.etag[1:-1] != key.etag[1:-1], "File already exists."
 
+        key = self.bucket.new_key(item.name)
+        key.set_contents_from_string(item.get_contents_as_string())
 
-def list_s3_keys(access_key_id, secret_access_key, host, tenant):
-    try:
-        conn = boto.connect_s3(
-            aws_access_key_id=access_key_id,
-            aws_secret_access_key=secret_access_key,
-            host=host,
-            is_secure=True,
-            calling_format=boto.s3.connection.OrdinaryCallingFormat()
-        )
-        bucket = conn.lookup('alaya-{}'.format(tenant))
-        assert bucket, "{}: tenant bucket not found.".format(tenant)
-        for key in bucket.list():
-            yield key
-    except Exception as ex:
-        logging.error('Unable to list S3 keys.')
-        logging.error(ex.message)
-        raise ex
+        key = self.bucket.get_key(item.name)
+
+        print('DST MD5: {}'.format(key.etag[1:-1]))
+        if item.etag[1:-1] != key.etag[1:-1]:
+            print('WARNING: source and destination hash don\'t match.')
+        else:
+            self.key_count += 1
+        return item
+
+    def after_process(self):
+        print("{} file(s) copied.".format(self.key_count))
 
 
 def args_spec():
     parser = argparse.ArgumentParser(
-        prog='ceph2swift.py',
+        prog='ceph2s3.py',
         description='Optional arguments can be defined as '
                     'environment variables.')
-    parser.add_argument('--ceph-key-id', type=str, action=EnvDefault,
-                        envvar='CEPH_KEY_ID')
-    parser.add_argument('--ceph-access-key', type=str, action=EnvDefault,
-                        envvar='CEPH_ACCESS_KEY')
-    parser.add_argument('--ceph-host', type=str, action=EnvDefault,
-                        envvar='CEPH_HOST')
 
-    parser.add_argument('--swift-auth-url', type=str, action=EnvDefault,
-                        envvar='SWIFT_AUTH_URL')
-    parser.add_argument('--swift-tenant-name', type=str, action=EnvDefault,
-                        envvar='SWIFT_TENANT_NAME')
-    parser.add_argument('--swift-user', type=str, action=EnvDefault,
-                        envvar='SWIFT_USER')
-    parser.add_argument('--swift-password', type=str, action=EnvDefault,
-                        envvar='SWIFT_PASSWORD')
+    parser.add_argument('--src-key-id', type=str, action=EnvDefault,
+                        envvar='SRC_KEY_ID')
+    parser.add_argument('--src-access-key', type=str, action=EnvDefault,
+                        envvar='SRC_ACCESS_KEY')
+    parser.add_argument('--src-host', type=str, action=EnvDefault,
+                        envvar='SRC_HOST')
+
+    parser.add_argument('--dst-key-id', type=str, action=EnvDefault,
+                        envvar='DST_KEY_ID')
+    parser.add_argument('--dst-access-key', type=str, action=EnvDefault,
+                        envvar='DST_ACCESS_KEY')
+    parser.add_argument('--dst-host', type=str, action=EnvDefault,
+                        envvar='DST_HOST')
+    parser.add_argument('--dst-region', type=str, action=EnvDefault,
+                        envvar='DST_REGION')
 
     parser.add_argument('--tenant', type=str, required=True)
 
     return parser
 
 
+_exit_signal = False
+
+
+def signal_handler(signal, frame):
+    print('User pressed Ctrl-C!')
+    global _exit_signal
+    _exit_signal = True
+
+
+def src_keys_generator(conn, tenant):
+    for key in conn.get_bucket('alaya-{}'.format(tenant)).list():
+        if _exit_signal:
+            raise StopIteration
+        yield key
+
+
 def main():
     parser = args_spec()
     args = parser.parse_args()
 
-    conn = swiftclient.client.Connection(
-        authurl=args.swift_auth_url,
-        user=args.swift_user,
-        key=args.swift_password,
-        tenant_name=args.swift_tenant_name,
-        auth_version=2
+    src_connection = boto.connect_s3(
+        aws_access_key_id=args.src_key_id,
+        aws_secret_access_key=args.src_access_key,
+        host=args.src_host,
+        is_secure=True,
+        calling_format=boto.s3.connection.OrdinaryCallingFormat()
     )
 
-    p = Pipeline(list_s3_keys(
-        args.ceph_key_id,
-        args.ceph_access_key,
-        args.ceph_host,
-        args.tenant
-    ))
+    dst_connection = boto.s3.connect_to_region(
+        args.dst_region,
+        aws_access_key_id=args.dst_key_id,
+        aws_secret_access_key=args.dst_access_key,
+        host=args.dst_host
+    )
 
-    p.add(S3KeyInfo())
-    p.add(Filter('exclude keys with \'default\' in the name.',
+    signal.signal(signal.SIGINT, signal_handler)
+
+    p = Pipeline(src_keys_generator(src_connection, args.tenant))
+
+    p.add(PrintFileInfo())
+    p.add(Filter('exclude keys with \'default\' in the name',
                  lambda x: 'default' in x.name))
-    p.add(SwiftCreateTenantContainer(connection=conn, tenant=args.tenant))
-    p.add(SwiftCreateFolderStructure(connection=conn, tenant=args.tenant))
-    p.add(Filter('exclude keys ending in \'\\\'',
-                 lambda x: x.name.endswith('/')))
-    p.add(SwiftCheckIfFileExists(connection=conn, tenant=args.tenant))
-    p.add(SwiftUploadFile(connection=conn, tenant=args.tenant))
 
-    p.run()
+    p.add(S3CreateFolderStructure(connection=dst_connection, tenant='testing'))
+    p.add(Filter('exclude keys ending in \'/\'', lambda x: x.name.endswith('/')))
+    p.add(S3UploadFile(connection=dst_connection, tenant='testing'))
+    p()
 
 if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG)
     main()
+
